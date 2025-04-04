ad086c57b2d89265fb42f87981f5df9d4738bcbf
muratokur01 <muratokur0504@gmail.com>
2025-04-04 22:47:41 +0300

    Add files via upload

---
+++ public/js/search.js
@@ -0,0 +1,10 @@
+const searchIcon = document.getElementById('searchIcon')
+const aramacubugu = document.getElementById('aramacubugu')
+
+searchIcon.addEventListener('click', ()=>{
+console.log('helloworld')
+aramacubugu.classList.toggle('active')
+
+   })
+
+   
